d845a925e24be60f8ffe3ec2633b3946542add3a
tom <de.cammie@gmail.com>
2019-08-02 11:38:51 +0200

    update helloworld

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello World new Commit");
 	}
 	
 	///hello-world/path-variable/in28minutes
